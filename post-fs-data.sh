011110f74318f202e7f0f10eade875dcf53b9261
silvzr <117378129+silvzr@users.noreply.github.com>
2026-01-16 21:08:19 +0100

    post-fs-data.sh: code cleanup
    Shamiko is deprecated, also we shouldn't forcefully edit users' denylist

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -8,19 +8,6 @@ if [ "$(magisk -V)" -lt 26302 ] || [ "$(/data/adb/ksud -V)" -lt 10818 ]; then
     touch "$MODPATH/disable"
 fi
 
-# Remove Play Services from Magisk DenyList when set to Enforce in normal mode
-if magisk --denylist status; then
-    magisk --denylist rm com.google.android.gms
-else # Check if Shamiko is installed and whitelist feature isn't enabled
-    if [ -d "/data/adb/modules/zygisk_shamiko" ] && [ ! -f "/data/adb/shamiko/whitelist" ]; then
-        magisk --denylist add com.google.android.gms com.google.android.gms.unstable
-    fi
-fi
-
-# magiskpolicy --live unsafe mrHuskyDG 😡
-# Permission Loophole...
-# Thanks to 7lpb3c for pointing it out ❤️
-
 # Using util_functions.sh
 [ -f "$MODPATH/util_functions.sh" ] && . "$MODPATH/util_functions.sh" || abort "! util_functions.sh not found!"
 
